14b979f2b64571dc58960130876a55a29cb72181
Jonathan <Jonathan@Jonathan-PC>
2015-07-28 11:23:40 +0200

    Table hardware

---
+++ fragments/hardwareTable.php
@@ -0,0 +1,58 @@
+<div class="col-lg-10 col-md-10 col-sm-10 col-xs-11" id="main">
+  <div class="panel panel-default table-responsive">
+      <div class="panel-heading">
+         Hardware 
+      </div>
+      <table class="table table-responsive">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Raum</th>
+                <th>Lieferant</th>
+                <th>Einkaufsdatum</th>
+                <th>Garantie in Jahren</th>
+                <th>Beschreibung</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>PC 004</td>
+                <td>102.110</td>
+                <td>Amazon</td>
+                <td>20.05.15</td>
+                <td>2</td>
+                <td>PC für Sekretärin</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>PC 004</td>
+                <td>102.110</td>
+                <td>Amazon</td>
+                <td>20.05.15</td>
+                <td>2</td>
+                <td>PC für Sekretärin</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>PC 004</td>
+                <td>102.110</td>
+                <td>Amazon</td>
+                <td>20.05.15</td>
+                <td>2</td>
+                <td>PC für Sekretärin</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>PC 004</td>
+                <td>102.110</td>
+                <td>Amazon</td>
+                <td>20.05.15</td>
+                <td>2</td>
+                <td>PC für Sekretärin</td>
+            </tr>
+        </tbody>
+      </table>
+  </div>                     
+</div>
