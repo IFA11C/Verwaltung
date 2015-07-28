da4db847671181eff9456b1578f103fb133ad7e9
schueler <schueler@WIN-3KDPHUGAAIB>
2015-07-28 10:19:06 +0200

    Globale Config für DB Conection

--- php/classes/db_connect.php
+++ php/classes/db_connect.php
@@ -1,47 +1,24 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Das ist der PHP-Code, den wir benutzen um uns mit der MySQL-Datenbank
+ * zu verbinden.
+ * 
+ * Inizalisieren des zentralen mysqli Objektes und verbinden mit der Datenbank
+ * Setzen des Charsets UTF-8
  */
 
-$server = 'localhost';
-$user = 'root';
-$pass = '';
-$database = 'itv_v02';
+include_once '/../config/#psl-config.php'; 
 
 //Verbindung aufbauen
-$db = new mysqli($server, $user, $pass, $database);
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 
-//Fehlerabfrage
-if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
+
+if ($mysqli->connect_error) {
+    header("Location: ./err.php?err=Unable to connect to MySQL");
+    exit();
+}
+if (!$mysqli->set_charset("utf8")) {
+    header("Location: ./err.php?err=Unable to set charset");
+    exit();
 }
 
-/*
-//Query aufbauen
-$sql = <<<SQL
-    SELECT *
-    FROM `raeume`
-SQL;
-
-//Query Fehlerabfrage
-if(!$result = $db->query($sql)){
-    die('There was an error running the query [' . $db->error . ']');
-}
-
-//Abfrage ausgeben
-while($row = $result->fetch_assoc()){
-    echo $row['r_id'] . '<br />';
-}
-
-//Anzahl erhaltener werte anzeigen lassen
-echo 'Total results: ' . $result->num_rows;
-
-//Results löschen
-$result->free();
-
-//Verbindung beenden
-$db->close();
- */
