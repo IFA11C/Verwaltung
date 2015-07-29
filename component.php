7e2840a9f879ef07229300d7b0aad0e07693c951
Jonathan <Jonathan@Jonathan-PC>
2015-07-29 09:34:42 +0200

    Viel Spaß Maximilian ;)

--- component.php
+++ component.php
@@ -82,7 +82,7 @@
             </div>
         </div>
 
-        <div class="modal fade modal-lg" id="modal-edit" tabindex="-1" role="dialog">
+        <div class="modal fade" id="modal-edit" tabindex="-1" role="dialog">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
