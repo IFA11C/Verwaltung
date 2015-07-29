cb284b7f9629ba40742c77248f22cdb363e13eae
Karen <Karen@Karen-PC>
2015-07-29 08:07:27 +0200

    Select für Auflistung aller Komponenten mit Standort und  Komponentenart

--- php/dbq/hardware.php
+++ php/dbq/hardware.php
@@ -12,7 +12,7 @@ include_once '/../classes/db_connect.php';
  * Diese Funktion gibt alle Hardware zurück 
  */
 function getHardware($mysqli) {
-    if (!$stmt = $mysqli->prepare("SELECT * FROM komponenten")) {
+    if (!$stmt = $mysqli->prepare("SELECT k.k_id, ka.ka_komponentenart, r.r_nr, k.k_einkaufsdatum, k.k_gewaehrleistungsdauer, k.k_hersteller, k.k_notiz FROM komponenten k inner join komponentenarten ka on k.komponentenarten_ka_id = ka.ka_id left join raeume r on r.r_id = k.raeume_r_id ")) {
         // Could not create a prepared statement
         header("Location: ./err.php?err=Database error: "
                 . "cannot prepare statement");
@@ -21,26 +21,25 @@ function getHardware($mysqli) {
     $stmt->execute();       
     $stmt->bind_result(
             $ID,       //Hardware ID
+            $TYPE,     //hardware type
             $ROOM ,        //Room ID
-            $VENDOR,       //Vendor ID
             $PDATE,        //Purchase Date
             $WARRANTY,    //Warranty (e.g. 2 for 2 years)
-            $NOTE,        //notes/notizen
             $MANUFACTURER,  //manufacturer
-            $TYPE);     //hardware type
+            $NOTE);        //notes/notizen
+            
     
     $hardware = array();
     
     while ($stmt->fetch()) {
         $hardware[] = array(
-            "Id" => $ID, 
-            "Room" => $ROOM, 
-            "Vendor" => $VENDOR, 
-            "PDate" => $PDATE, 
-            "Warranty" => $WARRANTY, 
-            "Note" => $NOTE, 
-            "Manufacturer" => $MANUFACTURER, 
-            "Type" => $TYPE);
+            "Id" => $ID,
+            "Type" => $TYPE,
+            "Room" => $ROOM,
+            "PDate" => $PDATE,
+            "Warranty" => $WARRANTY,
+            "Manufacturer" => $MANUFACTURER,
+            "Note" => $NOTE);  
     }
     return $hardware; 
 }
