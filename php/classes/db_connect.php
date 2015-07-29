ca4345f9c00e95733e981f6894890fe791571226
schueler <schueler@WIN-3KDPHUGAAIB>
2015-07-29 14:19:26 +0200

    Merge origin/master

--- php/classes/db_connect.php
+++ php/classes/db_connect.php
@@ -7,9 +7,9 @@
  * Setzen des Charsets UTF-8
  */
 
-define("HOST", "192.168.10.1"); 			// The host you want to connect to. 
-define("USER", "ultralord"); 		// The database username. 
-define("PASSWORD", "1234");                      // The database password. 
+define("HOST", "localhost"); 			// The host you want to connect to. 
+define("USER", "root"); 		// The database username. 
+define("PASSWORD", "");                      // The database password. 
 define("DATABASE", "itv_v02");                   // The database name.
 
 //Verbindung aufbauen
