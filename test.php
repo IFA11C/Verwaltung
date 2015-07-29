0393e79b81c140ec08782a443dd8b5aac8a81cac
Karen <Karen@Karen-PC>
2015-07-29 09:55:13 +0200

    Select für Auflistung alle KomponentenAttribute nach KomponentenID

--- test.php
+++ test.php
@@ -2,19 +2,37 @@
 include './php/classes/db_connect.php';
 include './php/dbq/rooms.php';
 include './php/dbq/hardware.php';
+include './php/dbq/hardwareAttribute.php';
 
+echo '<br/> <b>Räume:</b> <br/>';
 $rooms = getRooms($mysqli);
 foreach ($rooms as $room) {
     echo ' raum ' . $room["Id"] . '<br/>';
 }
+echo '<br/> <b>Hardware:</b> <br/>';
 $hardware = getHardware($mysqli);
-echo 'Hardware ID | Room | Purchase Date | Warranty | Manufacturer | Note |  Type <br/>';
+echo 'Hardware ID | Hardware Type | Room | Purchase Date | Warranty | Manufacturer | Note <br/>';
 foreach ($hardware as $item) {
     echo $item["Id"] . ' | ' . 
          $item["Type"] . ' | ' .   
          $item["Room"] . ' | ' .  
          $item["PDate"] . ' | ' .
-         $item["Warranty"] . ' | ' . 
+         $item["Warranty"] . ' Jahre | ' . 
          $item["Manufacturer"] . ' | ' .
          $item["Note"].'<br/>'; 
+}
+echo '<br/> <b>Hardware Attribute:</b> <br/>';
+$hardwareAttribute = getHardwareAttribute($mysqli, 1);
+echo 'Hardware ID | Hardware Type | Room | Purchase Date | Warranty | Manufacturer | Note | Attribut ID | Description | Value <br/>';
+foreach ($hardwareAttribute as $attribut) {
+    echo $attribut["Id"] . ' | ' . 
+         $attribut["Type"] . ' | ' .   
+         $attribut["Room"] . ' | ' .  
+         $attribut["PDate"] . ' | ' .
+         $attribut["Warranty"] . ' Jahre | ' . 
+         $attribut["Manufacturer"] . ' | ' .
+         $attribut["Note"] . ' | ' .
+         $attribut["AttributID"] . ' | ' .
+         $attribut["Description"] . ' | ' .
+         $attribut["Value"].'<br/>'; 
 }
